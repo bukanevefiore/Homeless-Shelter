cc5fcd09442cc4db44bc9c2ae327eed8d26e904c
bukanevefiore <59068120+bukanevefiore@users.noreply.github.com>
2021-03-04 15:08:07 +0300

    Add files via upload

---
+++ Models/Classes/Donation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.Classes
+{
+    public class Donation
+    {
+        [Key]
+        public int ID { get; set; }
+        public int userid { get; set; }
+        public int amount { get; set; }
+        public bool verification { get; set; }
+        public DateTime date { get; set; }
+    }
+}
